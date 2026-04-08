858ed2243f414c1f7b9ba283b07037901fec74aa
theTyai <work.mishraashish@gmail.com>
2026-04-09 04:25:17 +0530

    feat: update API base URL to production environment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://vision-oa-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
